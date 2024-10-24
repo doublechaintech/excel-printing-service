8c3b5a2ae9732d01047e8771db37f3e4b130a23c
Philip Zhang <zhangxilai@doublechaintech.com>
2024-10-25 02:38:25 +0800

    🚀finalize the code

--- src/excel_print_mac.py
+++ src/excel_print_mac.py
@@ -5,23 +5,7 @@ import tempfile
 import base64
 
 
-def printExcelFileInternal(fileName):
 
-    
-
-    tmp = tempfile.NamedTemporaryFile()
-   
-    resp=ExcelPrintingResonse()
-    resp.resultCode=0
-    resp.message="success with simulate mode"
-    content=base64.b64decode(request.content)
-    with open(tmp.name, 'w+b') as f:
-        f.write(content) # where `stuff` is, y'know... stuff to write (a string)
-    resp.fileLength = len(content)
-    resp.originalLength=len(request.content)
-    resp.fileName = tmp.name
-
-    return resp
     
 
 def printExcelFile(request:ExcelPrintingRequest):
